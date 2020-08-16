668aee48e5519c1538ee52803715d4f1a9942310
Alexander <al.kiri4enko2015@yandex.ru>
2020-08-17 00:23:38 +0300

    added new code

--- Rules/BaseRule.php
+++ Rules/BaseRule.php
@@ -6,11 +6,11 @@ use Rest\Handlers\IHandler;
 class BaseRule implements IRule
 {
     protected $templateURI;
+    protected $requestURI;
     protected $handler;
     protected $params;
     protected $ruleTemplate;
     protected $TemplateForParams = '/{[a-z A-Z 0-9]*}/';
-    protected $httpMethod = 'GET';
 
     public function __construct(string $uri, IHandler $handler){
         if(!empty($uri))
@@ -21,30 +21,32 @@ class BaseRule implements IRule
         $this->handler = $handler;
 
         $this->cutParamFromURI();
-        $this->ruleTemplate['default'] = '/[a-z A-Z 0-9]*/';
+        $this->ruleTemplate['default'] = '[a-z A-Z 0-9]*';
 
         return $this;
     }
 
-    public function match(string $uri) : bool{
+    public function match() : bool{
         $__uriTemplate = $this->replaceParamsInUri();
 
-        if(preg_match($__uriTemplate, $uri))
+        if(preg_match('/^'.$__uriTemplate.'$/', $this->requestURI))
             return true;
         return false;
     }
 
-    protected function replaceParamsInUri() : string{
-        $newUri = $this->templateURI;
+    protected function replaceParamsInUri() : string {
+        $newUri = str_replace('/', '\/', $this->templateURI);
+
         foreach ($this->params as $param){
-            $paramCp = '{'.$param.'}';
             if(in_array($param, array_keys($this->ruleTemplate)))
-                $regex = $this->ruleTemplate[$paramCp];
+                $regex = $this->ruleTemplate[$param];
             else
                 $regex = $this->ruleTemplate['default'];
 
-            $newUri = str_replace($paramCp, $regex);
+            $newUri = str_replace('{'.$param.'}', $regex, $newUri);
         }
+
+        return $newUri;
     }
 
     public function where(string $param, string $regex) : IRule{
@@ -56,6 +58,8 @@ class BaseRule implements IRule
         }
         else
             throw new Exception('param '.$param.' not found');
+
+        return $this;
     }
 
     public function delegate(){
@@ -66,12 +70,32 @@ class BaseRule implements IRule
         preg_match_all($this->TemplateForParams, $this->templateURI, $params, PREG_PATTERN_ORDER);
 
         foreach ($params as $key => $param)
-            d($params);
-            $this->params[] = trim($param, '{}');
+            foreach ($param as $key => $param)
+                $this->params[] = trim($param, '{}');
     }
 
-    public function getHttpMethod(): string
-    {
-        return $this->httpMethod;
+    public function getParamsValueFromURI() : array{
+        $templateReg = $this->replaceParamsInUri();
+
+        $values = [];
+        $arParamToVAlue = [];
+        $count = 0;
+
+        preg_match_all('/'.$templateReg.'/', $this->requestURI, $values, PREG_PATTERN_ORDER);
+
+        d($templateReg, $this->requestURI, $values);
+
+        foreach ($this->params as $param){
+            $arParamToVAlue[$param] = $values[0][$count];
+        }
+
+        return [];
+    }
+
+    public function setRequestURI(string $uri){
+        if(!empty($uri))
+            $this->requestURI = $uri;
+        else
+            throw new Exception("uri is empty");
     }
 }
--- Rules/IRule.php
+++ Rules/IRule.php
@@ -7,8 +7,9 @@ use Rest\Handlers\IHandler;
 interface IRule
 {
     public function __construct(string $uri, IHandler $handler);
-    public function match(string $uri) : bool;
+    public function match() : bool;
     public function where(string $param, string $regex) : IRule;
     public function delegate();
-    public function getHttpMethod() : string;
+    public function setRequestURI(string $uri);
+    public function getParamsValueFromURI() : array;
 }
